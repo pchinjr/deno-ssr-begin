e36ba33c149b35ce84b66338611f4e046cba8dd0
Begin <support@begin.com>
2021-03-01 14:57:45 +0000

    ci: so it Begins!

---
+++ app.arc
@@ -0,0 +1,11 @@
+@app
+deno-ssr
+
+@http
+get /
+
+@static
+
+# @aws
+# profile default
+# region us-west-1
---
+++ src/http/get-index/components/main.js
@@ -0,0 +1,9 @@
+export default function Main(state={}) {
+
+  return `
+  <div>
+  <h1> Praise Cage </h1>
+  <p> Time to pray, it is now <span id='time'> </span> </p>
+  </div>
+  `
+}
